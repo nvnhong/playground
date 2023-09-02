82c9fdea33336d99034f527dbf00b82ec15f1914
hongmikyeong <mikyeong.dev@gmail.com>
2023-09-02 20:41:12 +0900

    Popup.jsx 파일명 변경 -> PopupWithButtons.jsx

--- UI/src/components/Modal/PopupWithButtons.jsx
+++ UI/src/components/Modal/PopupWithButtons.jsx
@@ -1,6 +1,6 @@
 import { styled } from "styled-components";
 
-export default function Popup({ handleClick }) {
+export default function PopupWithButtons({ handleClick }) {
   return (
     <>
       <BackGround />
